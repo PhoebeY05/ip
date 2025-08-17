163d0a012a9f9bd3549c633b8060bb8838e31d93
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2025-08-17 17:12:16 +0800

    Add support for echo

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("------------------------------------");
         System.out.println("Hello! I'm ChatBot!");
         System.out.println("What can I do for you?");
         System.out.println("------------------------------------");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("------------------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println("------------------------------------");
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("------------------------------------");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println("------------------------------------");
+            }
+        }
     }
 }

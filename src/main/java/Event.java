51eb4a5ae4404a1bb77a99bff3ef1761f6c657b4
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2025-08-25 22:25:54 +0800

    feat: Add datetime handling to tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, DateTimeFormatter.ofPattern("d/M/yyyy HHmm"));
+        this.to = LocalDateTime.parse(to, DateTimeFormatter.ofPattern("d/M/yyyy HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        String dateFrom = this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy, HH:mm"));
+        String dateTo = this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy, HH:mm"));
+        return "[E]" + super.toString() + " (from: " + dateFrom + " to: " + dateTo + ")";
     }
 }

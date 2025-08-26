3d8d42739a4deaf133bf3c09aa02c09b84d621fd
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2025-08-26 22:04:19 +0800

    feat: Add TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this(new ArrayList<Task>());
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void deleteTask(Task task) {
+        this.tasks.remove(task);
+    }
+}

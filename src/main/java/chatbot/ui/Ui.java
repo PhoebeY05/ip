5a203721ce9fb131ca5c761e81c8b71c391f072b
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2025-08-28 11:50:56 +0800

    refactor: Change code to follow coding standards in Ui

--- src/main/java/chatbot/ui/Ui.java
+++ src/main/java/chatbot/ui/Ui.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 public class Ui {
 
     public boolean handleInput(Parser parser, TaskList tasks) throws ChatBotException {
-
         System.out.println("------------------------------------");
+
         CommandType commandType = parser.getCommandType();
         Task addedTask = null;
         ArrayList<String> args = parser.getArguments();
@@ -20,38 +20,48 @@ public class Ui {
             case BYE:
                 this.endConversation();
                 return false;
+
             case LIST:
                 this.listTasks(tasks);
                 break;
+
             case MARK:
-                Task tDone = parser.getTask(tasks);
-                tDone.markAsDone();
-                this.showMarkedAsDone(tDone);
+                Task taskToMark = parser.getTask(tasks);
+                taskToMark.markAsDone();
+                this.showMarkedAsDone(taskToMark);
                 break;
+
             case UNMARK:
-                Task tUndone = parser.getTask(tasks);
-                tUndone.markAsUndone();
-                this.showMarkedAsUndone(tUndone);
+                Task taskToUnmark = parser.getTask(tasks);
+                taskToUnmark.markAsUndone();
+                this.showMarkedAsUndone(taskToUnmark);
                 break;
+
             case DELETE:
-                Task tDeleted = parser.getTask(tasks);
-                tasks.deleteTask(tDeleted);
-                this.showDeleted(tDeleted, tasks.getTotalTasks());
+                Task taskToDelete = parser.getTask(tasks);
+                tasks.deleteTask(taskToDelete);
+                this.showDeleted(taskToDelete, tasks.getTotalTasks());
                 break;
+
             case TODO:
                 System.out.println("Got it. I've added this task:");
                 addedTask = new Todo(args.get(0));
                 break;
+
             case DEADLINE:
                 System.out.println("Got it. I've added this task:");
                 addedTask = new Deadline(args.get(0), args.get(1));
                 break;
+
             case EVENT:
                 System.out.println("Got it. I've added this task:");
                 addedTask = new Event(args.get(0), args.get(1), args.get(2));
                 break;
+
             default:
-                throw new ChatBotException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                throw new ChatBotException(
+                        "OOPS!!! I'm sorry, but I don't know what that means :-("
+                );
         }
 
         if (addedTask != null) {
@@ -72,25 +82,25 @@ public class Ui {
         System.out.println(tasks);
     }
 
-    public void showMarkedAsDone(Task t) {
+    public void showMarkedAsDone(Task task) {
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println(t);
+        System.out.println(task);
     }
 
-    public void showMarkedAsUndone(Task t) {
+    public void showMarkedAsUndone(Task task) {
         System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(t);
+        System.out.println(task);
     }
 
-    public void showDeleted(Task t, int size) {
+    public void showDeleted(Task task, int totalTasks) {
         System.out.println("Noted. I've removed this task:");
-        System.out.println(t);
-        System.out.printf("Now you have %d task(s) in the list.\n", size);
+        System.out.println(task);
+        System.out.printf("Now you have %d task(s) in the list.\n", totalTasks);
     }
 
-    public void showAddedTask(Task t, int size) {
-        System.out.println(t);
-        System.out.printf("Now you have %d task(s) in the list.\n", size);
+    public void showAddedTask(Task task, int totalTasks) {
+        System.out.println(task);
+        System.out.printf("Now you have %d task(s) in the list.\n", totalTasks);
     }
 
     public void showLoadingError() {
